4de139add978687c7ae778b2ee52503f8c450761
Danijela <danijelaherceg079@gmail.com>
2019-01-24 20:09:07 +0100

    Inittial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+
+       public static void main(String[] args) {
+     
+        System.out.println("Hello World"); 
+        System.out.println("Danijela"); 
+        
+        String user = "Danijela Herceg";
+        
+        Car audi = new Car();
+        
+        audi.printVariables();
+        
+     
+           
+    }
+    
+}
